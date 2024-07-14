d189351348fff4f6fed1a73437ed590d5f4b243e
anamerasul <anamerasul@gmail.com>
2024-07-15 03:15:20 +0600

    all

---
+++ server.js
@@ -0,0 +1,28 @@
+const dgram = require('dgram');
+const stun = require('stun');
+
+const server = dgram.createSocket('udp4');
+
+server.on('message', (msg, rinfo) => {
+    const message = stun.decode(msg);
+
+    if (message.method === stun.constants.methods.BINDING) {
+        const response = stun.createMessage(stun.constants.methods.BINDING, message.transactionId);
+        response.addXorAddress(rinfo.address, rinfo.port);
+
+        server.send(response.toBuffer(), rinfo.port, rinfo.address, (err) => {
+            if (err) {
+                console.error('Error sending response:', err);
+            } else {
+                console.log('Response sent to', rinfo.address, rinfo.port);
+            }
+        });
+    }
+});
+
+server.on('listening', () => {
+    const address = server.address();
+    console.log(`STUN server listening on ${address.address}:${address.port}`);
+});
+
+server.bind(3478, '127.0.0.1');
